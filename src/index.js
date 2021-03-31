822f9f27f4305dc4ea41b8957ae14d002e2d6f43
Isela <isela.v.alarcon@gmail.com>
2021-03-31 20:32:28 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 //create a react component
 
 const App = () => {
-  return <div>Hi there! </div>;
+  return <div>Hi there!</div>;
 };
 //Take the react component and show it on the screen
 
